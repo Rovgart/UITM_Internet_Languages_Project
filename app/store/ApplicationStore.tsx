b2a55b71a94fadebe040e9cb709e44aef2dd3826
Daniel Niedzielski <daniel.niedziels@wp.pl>
2024-08-31 16:05:46 +0200

    Added zustand auth store

--- app/store/ApplicationStore.tsx
+++ app/store/ApplicationStore.tsx
@@ -1,3 +1,16 @@
+import { Token } from "@/types/types";
 import { create } from "zustand";
-
-const useStore = create((set) => {});
+type UserCredentials = {
+  email: string;
+  password: string;
+  rememberMe: boolean;
+};
+interface AuthStore {
+  accessToken: string | null;
+  refreshToken: string | null;
+  user: any | null;
+  isAuthenticated: boolean;
+  login: (formValues: UserCredentials) => Promise<Token | undefined>;
+  refreshAccessToken: () => Promise<void>;
+}
+const useStore = create<AuthStore>();
